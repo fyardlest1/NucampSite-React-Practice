8cc06b6c229f3db9915d30c73790a438151d60b9
Fyardlest <fyardlest1@gmail.com>
2020-06-01 21:46:15 -0400

    Functional Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import {
   Card,
   CardImg,
-  CardImgOverlay,
   CardText,
   CardBody,
   CardTitle,
@@ -48,15 +47,15 @@ class CampsiteInfo extends Component {
     render() {
         if (this.props.campsite) {
             return (
-                <div className='row'>
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             );
-        } 
-        return (
-            <div />
-        );
+        }
+        return <div />;
     }
 
 }
